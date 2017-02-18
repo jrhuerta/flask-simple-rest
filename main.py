84825582e2cd846ab3653982323979af4fb475e4
Joaquin A. Rodriguez Huerta <jrhuerta@alayacare.com>
2017-02-18 15:17:04 -0500

    Add basic pagination functionality.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import functools
+import logging
 
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 from marshmallow import fields, Schema, post_load, validates, ValidationError
 from math import ceil
-from werkzeug.utils import cached_property
 
 
 app = Flask(__name__)
@@ -20,6 +20,18 @@ def validation_error_handler(error):
     """Transforms validation error messages in a json response."""
     response = jsonify(error.messages)
     response.status_code = 400
+    return response
+
+
+@app.errorhandler(Exception)
+def validation_error_handler(error):
+    """Transforms any other error into a json response without leaking data."""
+    logging.exception(error)
+    response = jsonify({
+        'message': 'Oops we did it again...',
+        'error': repr(error)
+    })
+    response.status_code = 500
     return response
 
 
@@ -81,7 +93,38 @@ class ProductCreateSchema(Schema):
         return Product(**data)
 
 
-def marshall_with(schema, **kwargs):
+def positive_int(value):
+    rv = int(value)
+    if not rv > 0:
+        raise ValueError("{}: is not a positive integer.".format(rv))
+    return rv
+
+
+def paginate_result(query, serializer):
+    """Very simple pagination helper.
+    This logic could be more elaborated and moved into a class.
+    """
+    parser = reqparse.RequestParser(bundle_errors=True)
+    parser.add_argument('page', type=positive_int, default=1,
+                        help='Page number must be a positive integer.')
+    parser.add_argument('count', type=positive_int, default=5, required=False,
+                        help='Items per page must be a positive integer.')
+    args = parser.parse_args()
+
+    total_pages = int(ceil(query.count() / float(args.count)))
+    if args.page > total_pages:
+        args.page = total_pages
+
+    return {
+        'page': args.page,
+        'items_per_page': args.count,
+        'total_pages': total_pages,
+        'items': serializer(
+            query.offset((args.page - 1) * args.count).limit(args.count)).data
+    }
+
+
+def marshall_with(schema, pagination=False, **kwargs):
     """Decorator to serialize output using specified schema
     :param kwargs will be passed down to the dump method from marshmallow Schema
     """
@@ -91,7 +134,11 @@ def marshall_with(schema, **kwargs):
         @functools.wraps(f)
         def inner(*fargs, **fkwargs):
             rv = f(*fargs, **fkwargs)
-            return jsonify(serializer(rv).data)
+            if pagination:
+                rv = paginate_result(rv, serializer)
+            else:
+                rv = serializer(rv).data
+            return jsonify(rv)
         return inner
     return decorator
 
@@ -119,7 +166,7 @@ class ProductCollection(Resource):
     def __init__(self, repository_factory):
         self.repository = repository_factory()
 
-    @marshall_with(ProductCollectionSchema(), many=True)
+    @marshall_with(ProductCollectionSchema(), pagination=True, many=True)
     def get(self):
         return self.repository.get_all()
 
