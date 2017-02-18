45194e212cddfe151ee35c63d3d381592b556959
Joaquin A. Rodriguez Huerta <jrhuerta@alayacare.com>
2017-02-18 00:10:57 -0500

    Adding product model.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
+
+db = SQLAlchemy(app)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Unicode, nullable=False)
+    inventory = db.Column(db.Integer, nullable=False)
 
 
 @app.route('/')
@@ -9,4 +20,5 @@ def index():
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(host='0.0.0.0', debug=True)
